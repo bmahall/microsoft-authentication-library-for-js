1d52d0856169f13777b1b402fe1ecdb06f0a6b47
Balraj Mahal <bmahal@microsoft.com>
2021-11-04 17:03:33 -0400

    rel3

--- release-scripts/release_date.js
+++ release-scripts/release_date.js
@@ -22,3 +22,4 @@ if (dateOfNextRelease > 7) {
 const delimiter = noSpaces ? '-' : ' ';
 const dateComponents = [months[month], year]
 const formattedDate = dateComponents.join(delimiter)
+console.log(formattedDate) //returns formatted date
